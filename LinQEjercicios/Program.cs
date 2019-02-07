863fb92dd382369d7357cb26f40610de18ef8960
Jorge Mercado Ferreño <jorgemercadof@hotmail.com>
2019-02-07 02:27:35 +0100

    ...

---
+++ LinQEjercicios/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace LinQEjercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Preguntar por todos los casos ordenados por estatus
+
+            //TODO: Preguntar por todos los casos en los que es estatus sea A
+
+            //TODO: Preguntar por todos los casos que en los que la duracíón es mayor que 15 minutos, ordenador por estatus
+
+            //TODO: Preguntar por todos los casos que son de la subcategoria 7 ordenados por duración de forma descendente
+
+            //TODO: Preguntar por todas las subcategorias que pertenecen a una categoria, me explico:
+            // Categoria: Id: 2 Nombre: Informatica, 
+            //            SubCategoria { Id = 3, IdCategoria = 2, Nombre = "Instalacion de Aplicación"},
+            //            SubCategoria { Id = 4, IdCategoria = 2, Nombre = "Virus" },
+            //            SubCategoria { Id = 7, IdCategoria = 2, Nombre = "Solicitud de Equipos" },
+
+            //TODO: Determinar para cada subcategoria, que casos hay, ordenados por duración y estado
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+// Prueba GitHub
